bc6283444e794078d491b97d66a1dbd4f9e58c63
oguzhanozgokce <oguzhanozgokce@teknasyon.com>
2026-07-16 12:37:32 +0300

    feat: add showToast extension to Context for displaying messages

--- library/common/src/main/java/app/oguzhanozgokce/midmoney/common/extensions/Extensions.kt
+++ library/common/src/main/java/app/oguzhanozgokce/midmoney/common/extensions/Extensions.kt
@@ -1,8 +1,15 @@
 package app.oguzhanozgokce.midmoney.common.extensions
 
+import android.content.Context
+import android.widget.Toast
+
 fun Int?.orZero(): Int = this ?: 0
 fun Long?.orZero(): Long = this ?: 0L
 fun Double?.orZero(): Double = this ?: 0.0
 fun Float?.orZero(): Float = this ?: 0f
 fun Boolean?.orFalse(): Boolean = this ?: false
 fun Boolean?.orTrue(): Boolean = this ?: true
+
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
+}
